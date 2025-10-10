4d9a0fcdce494e35bfad7edfe9cb469aeb6d1fa0
nbarrellon <nbarrellon@montaigne-paris.fr>
2025-10-10 13:57:25 +0200

    Auto-sync: file changes

---
+++ MINIPROJET1/multiplex/plusieursclientsMP.py
@@ -0,0 +1,97 @@
+#Nils BARRELLON 21401602
+#CLIENTS QUI EMPRUNTENT LE TUNNEL DE RELAI
+
+from socket import *
+from threading import *
+
+#-------------------------------------------------------------
+# Informations de connexion au relai
+serverName = '127.0.0.1'
+serverPort = 55551
+
+def client(fichier,client):
+    print("------------Client n°",client,"------------")
+    print("Je demande l'URI :",fichier)
+    print("-------------------------------------------")
+    def lire_reponse(clientSocket):
+    # Lire la réponse HTTP ligne par ligne pour récupérer l'en-tête (de longueur variable selon la réponse)
+        reponse = b""
+        while True:
+            data = clientSocket.recv(1024)
+            #print(data)
+            if not data:
+                break
+            reponse += data
+            # On s'arrête si on a lu la fin des en-têtes (ligne vide)
+            #il est possible qu'on ait récupéré un peu du corps du message
+            if b"\r\n\r\n" in reponse:
+                break
+        # On sépare les en-têtes du corps
+        # Ignorer les 4 premiers octets (taille du message)
+        if len(reponse) >= 4:
+            reponse = reponse[4:]  # On saute les 4 premiers octets qui sont la taille du fichier envoyé en amont
+        en_tete, corps = reponse.decode('utf-8').split("\r\n\r\n", 1)
+        # Analyser les différents champs de l'en-tête
+        headers = en_tete.split("\r\n")
+        status = headers[0]
+        print("Réponse HTTP : ",status)
+        # Extraire la taille du contenu
+        longueur = 0
+        for header in headers[1:]:
+            if header.lower().startswith("content-length:"):
+                longueur = int(header.split(":")[1].strip()) 
+        print("Taille en octets du contenu:",longueur)
+        # Lire le reste du corps si nécessaire
+        if longueur > 0:
+        # Calculer la taille du corps déjà reçu
+            restant = longueur - len(corps)
+            # Lire le reste du corps
+            if restant > 0:
+                corps += recvall(clientSocket,restant)
+            #ecrire le corps dans un nouveau fichier qui porte le même nom que celui demandé + "_copy"
+            nom_fichier,extension = tuple(fichier.split('.'))
+            #si le fichier n'est pas interdit, on en enregistre une copie dans le répertoire courant
+            if "WARNING" not in corps:
+                if "STATS" in fichier: #si les stats ont été demandées
+                    fichier_copy = nom_fichier+"_stats."+extension.replace(" STATS","")
+                else:
+                    fichier_copy = nom_fichier+"_copy."+extension
+                if status[9:12]=="200":
+                    print("Sauvegarde sous le nom :",fichier_copy)
+                    with open(fichier_copy,"w") as f:
+                        f.write(corps)
+            else:
+                print("FICHIER INTERDIT D'ACCES")
+        else:
+            print("Aucun contenu reçu.")    
+    clientSocket = socket(AF_INET,SOCK_STREAM) #TCP
+    try:
+        clientSocket.connect((serverName,serverPort))
+        #----------------------------------------------
+        requete = "GET " + fichier
+        requete = requete.encode('utf-8')
+        clientSocket.send(requete)
+        try:
+            lire_reponse(clientSocket)
+        except Exception as e:
+            print(f"Erreur : {e}")
+    except ConnectionRefusedError:
+        print("Impossible de se connecter au relai.")
+    except ConnectionResetError:
+        print("La connexion a été fermée par le serveur.")
+    except socket.timeout:
+        print("Timeout : Le serveur ne répond pas.")
+    except Exception as e:
+        print(f"Erreur : {e}")
+    finally:
+        print("Fermeture du tunnel TCP")
+        clientSocket.close()
+#-------------------------------------------------------------------
+# on cree autant de thread que de client
+from random import randint
+requete = ['film1.txt',"foo.txt","foo.txt STATS","cochon.txt","cochon.txt STATS","musique1.txt","musique1.txt STATS"]
+for n_client in range(5):
+    i = randint(0,len(requete)-1)
+    Thread(target=client,args=(requete[i],n_client)).start()
+    
+           
--- MINIPROJET1/multiplex/serveurMP.py
+++ MINIPROJET1/multiplex/serveurMP.py
@@ -1,10 +1,24 @@
 from select import *
 from socket import *
 
+
+def get_local_ip():
+    s = socket(AF_INET, SOCK_DGRAM)
+    try:
+        # On se connecte à un serveur externe (non résolu) pour obtenir l'IP locale
+        s.connect(("8.8.8.8", 80))
+        local_ip = s.getsockname()[0]
+    except Exception:
+        local_ip = "127.0.0.1"
+    finally:
+        s.close()
+    return local_ip
+
 my_poll = poll()
 serverPort = 8080 #en lieu et place du port habituel 80
-serverSocket = socket(AF_INET,SOCK_STREAM) 
-my_poll.register(serverSocket,POLLIN)
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(('', serverPort))  # '' signifie "toutes les interfaces réseau disponibles"
+serverSocket.listen()
 sockets = {serverSocket.fileno(): serverSocket}
 # retrieve socket object from fileno
 received = dict()
@@ -12,6 +26,10 @@ received = dict()
 to_send = dict()
 # bytes to be sent from fileno, that have been processed
 while True:
+    print("-----------------------------------------------")
+    print("Serveur en écoute sur l'adresse IP", get_local_ip())
+    print("Port d'écoute :",serverPort)
+    print("----------------------------------------------")
     for fd, event in my_poll.poll():
         if event & (POLLHUP|POLLERR|POLLNVAL):
             received.pop(fd)
