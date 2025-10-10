6da70dce3296adbb98c19c7a01b90466f44181aa
nbarrellon <nbarrellon@montaigne-paris.fr>
2025-10-10 13:53:09 +0200

    Auto-sync: file changes

--- MINIPROJET1/multiplex/testmultiplex.py
+++ MINIPROJET1/multiplex/testmultiplex.py
@@ -1,19 +1,51 @@
 from select import *
+from socket import *
 
+my_poll = poll()
+serverPort = 8080 #en lieu et place du port habituel 80
+serverSocket = socket(AF_INET,SOCK_STREAM) 
+my_poll.register(serverSocket,POLLIN)
+sockets = {serverSocket.fileno(): serverSocket}
+# retrieve socket object from fileno
+received = dict()
+# bytes received from fileno, that are not yet processed
+to_send = dict()
+# bytes to be sent from fileno, that have been processed
 while True:
-
-    my_poll = poll()
-    my_poll.register(serverSocket,POLLIN)
-    sockets = {serverSocket.fileno(): serverSocket}
-    # retrieve socket object from fileno
     for fd, event in my_poll.poll():
-    if event & (POLLHUP|POLLERR|POLLNVAL):
-    received.pop(fd)
-    to_send.pop(fd)
-    received = dict()
-    # bytes received from fileno, that are not yet processed
-    to_send = dict()
-    my_poll.unregister(fd)
-    del sockets[fd]
-    # bytes to be sent from fileno, that have been processed
-    sockets.pop(fd)
+        if event & (POLLHUP|POLLERR|POLLNVAL):
+            received.pop(fd)
+            to_send.pop(fd)
+            my_poll.unregister(fd)
+            del sockets[fd]
+            sockets.pop(fd)
+        elif sockets[fd] is serverSocket:
+            connectionSocket, address = serverSocket.accept()
+            sockets[connectionSocket.fileno()] = connectionSocket
+            my_poll.register(connectionSocket,POLLIN)
+        else:
+            if event & POLLIN:
+                try:
+                    data = sockets[fd].recv(4096)
+                    if not data:
+                        sockets[fd].close()
+                        continue
+                    if fd in received:
+                        received[fd] += data
+                    else:
+                        received[fd] = data
+                    my_poll.modify(fd,POLLIN|POLLOUT)
+                except Exception as e:
+                    print(f"Erreur de socket : {e}")
+                    sockets[fd].close()
+                    continue
+            if event & POLLOUT:
+                data = received.pop(fd).decode("utf-8")
+                data = data.upper().encode("utf-8")
+                if fd in to_send:
+                    data = to_send.pop(fd) + data
+                n = sockets[fd].send(data)
+                if n < len(data):
+                    to_send[fd] = data[n:]
+                else:
+                    my_poll.modify(fd,POLLIN)
